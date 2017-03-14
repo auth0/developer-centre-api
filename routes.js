ac14e643e9a235aa39be5f6ffeca74f1b429f714
PROSPER OTEMUYIWA <prosperotemuyiwa@gmail.com>
2017-03-14 08:43:24 +0100

    Remove unecessary routes

--- routes.js
+++ routes.js
@@ -12,9 +12,6 @@ module.exports = function(app) {
     res.json("Welcome to the Developer Centre Api");
   });
 
-  // app.post('/api/login',    User.authenticateUserByEmail);
-  // app.post('/api/register', User.registerUser);
-
   app.get('/api/clients',  Client.getClients);
   // app.get('/api/user/:user_id',    User.getEachUserDetails);
   // app.get('/api/users/:username',  User.getEachUserByUsername);
@@ -22,19 +19,4 @@ module.exports = function(app) {
   app.delete('/api/client/:client_id', Client.deleteClient);
 
   app.post('/api/client', Client.create);
-  // app.post('/api/contact',     Contact.sendMessage);
-  // app.post('/api/newsletter',  Newsletter.subscribe);
-  // app.post('/api/password',    User.resetUserPassword);
-
-  // app.post('/api/jobs/create', Job.create);
-  // app.get('/api/jobs', Job.getAllJobs);
-
-  // app.post('/api/project', Project.shareProject);
-  // app.get('/api/project',  Project.getAllProjects);
-  // app.get('/api/projects/:projectSlug', Project.getEachProjectDetail);
-  // app.delete('/api/project/:id', verifyToken, Project.deleteEachProject);
-
-  // app.post('/api/tutorials/create', verifyToken, Tutorial.create);
-  // app.get('/api/tutorials', Tutorial.getAllTutorials);
-  // app.get('/api/tutorials/:slug', Tutorial.getEachTutorialDetails);
 };
